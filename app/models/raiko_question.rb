70b638046ca5196cf8326fd1493f379a80c0936a
Ryo Kato <katoryo55@gmail.com>
2012-06-09 03:01:28 +0900

    delete files

--- app/models/raiko_question.rb
+++
@@ -1,3 +0,0 @@
-class RaikoQuestion < ActiveRecord::Base
-  attr_accessible :query
-end
--- db/migrate/20120530112017_create_raiko_questions.rb
+++
@@ -1,9 +0,0 @@
-class CreateRaikoQuestions < ActiveRecord::Migration
-  def change
-    create_table :raiko_questions do |t|
-      t.text :query
-
-      t.timestamps
-    end
-  end
-end
